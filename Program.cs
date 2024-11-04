c90480a6b2adb6485fe956ee0f63c3e2b33da846
Melih Koçak <melih3660k@gmail.com>
2024-11-04 14:16:04 +0300

    'CARD' sınıfı oluşturuldu ve ilgili fieldlare kapsülleme işlemi yapıldı

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class CARD
+{
+    #region VARIABLES
+    float width, height, thickness;
+    #endregion
+
+    #region WIDTH PROP
+    public float WIDTH
+    {
+        get { return width; }
+        set { width = value; }
+    }
+    #endregion
+
+    #region HEIGHT PROP
+    public float HEIGHT
+    {
+        get { return height; }
+        set { height = value; }
+    }
+    #endregion
+
+    #region THICKNESS PROP
+    public float THICKNESS
+    {
+        get { return thickness; }
+        set { thickness = value; }
+    }
+    #endregion
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
