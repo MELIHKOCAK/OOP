2522543495cc1da249933843860d6f6a76af0fe2
Melih Koçak <melih3660k@gmail.com>
2024-11-05 22:06:06 +0300

    IDENTITY CARD SINIFI EKLENDİ AD, SOYAD, BIRTHDAY PROPLARI EKLENDİ

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 ﻿class CARD
 {
+    #region CONSTRUCTOR
+    public CARD(double width, double height, double thickness)
+    {
+        WIDTH = width;
+        HEIGHT = height;
+        THICKNESS = thickness;
+    }
+    #endregion
+
     #region VARIABLES
-    float width, height, thickness;
+    double width, height, thickness;
     #endregion
 
     #region WIDTH PROP
-    public float WIDTH
+    public double WIDTH
     {
         get { return width; }
         set { width = value; }
@@ -13,7 +22,7 @@
     #endregion
 
     #region HEIGHT PROP
-    public float HEIGHT
+    public double HEIGHT
     {
         get { return height; }
         set { height = value; }
@@ -21,14 +30,83 @@
     #endregion
 
     #region THICKNESS PROP
-    public float THICKNESS
+    public double THICKNESS
     {
         get { return thickness; }
         set { thickness = value; }
     }
     #endregion
+
+
 }
 
+class IDENTITYCARD : CARD
+{
+    #region CONSTRUCTOR
+    public IDENTITYCARD() : base(8.3, 5.1, 0.1)
+    {
+        WIDTH = base.WIDTH;
+        HEIGHT = base.HEIGHT;
+        THICKNESS = base.THICKNESS;
+    }
+    #endregion
+
+    #region VARIABLES
+    bool control;
+    string name, surname, result;
+    DateTime birthDay;
+    #endregion
+
+    #region NAME PROP
+    public string NAME
+    {
+        get { return name; }
+        set { name = value.ToUpper(); }
+    }
+
+    #endregion
+
+    #region SURNAME PROP
+    public string SURNAME
+    {
+        get { return surname; }
+        set { surname = value.ToUpper(); }
+    }
+
+    #endregion
+
+    #region BIRTHDAY PROP
+    public string BIRTHDAY
+    {
+        get
+        {
+            if (control == true)
+            {
+                result = "Please, check your birthday";
+                return result;
+            }
+            else
+            {
+                return birthDay.ToShortDateString();
+            }
+        }
+        set
+        {
+            control = DateTime.Compare(DateTime.Now, Convert.ToDateTime(value)) < 0;
+            if (control == true)
+            {
+                Console.WriteLine("Please, check your birthday");
+            }
+            else
+            {
+                birthDay = Convert.ToDateTime(value);
+            }
+
+        }
+    }
+    #endregion
+
+}
 
 internal class Program
 {
